38bde9be595eb3c5655b0dad4e6462aa50da4038
Veljko Veljovic <veljko.veljovic@vegait.rs>
2024-11-08 16:03:55 +0100

    mediatr library

---
+++ BuildingBlocks/CQRS/ICommand.cs
@@ -0,0 +1,19 @@
+﻿using MediatR;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BuildingBlocks.CQRS
+{
+    public interface ICommand : ICommand<Unit>
+    {
+
+    }
+
+    //defines a contract for all command types
+    public interface ICommand <out TResponse> : IRequest<TResponse>
+    {
+    }
+}
